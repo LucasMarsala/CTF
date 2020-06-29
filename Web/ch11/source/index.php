e78f27079fa1d4cd892d927ae88f0b90a447f741
Lucas <lucas.marsala@epitech.eu>
2020-06-29 16:50:01 +0200

    summer camp test

---
+++ Web/ch11/source/index.php
@@ -0,0 +1,129 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Home</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body id="page-top">
+        <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-secondary text-uppercase" id="mainNav">
+            <div class="container"><a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/home.png" id="home"></a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" aria-controls="navbarResponsive"
+                    aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1" role="presentation"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="rQ6m|&Y4.php" style="color: black; background-color: #cbaf4b">Register</a></li>
+                        <li class="nav-item mx-0 mx-lg-1" role="presentation"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php" disbale="disbale" style="color: black; background-color: #cbaf4b">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="masthead text-white text-center">
+            <div class="container">
+                <h1><span style="color: black">GOD</span> ITAKALA <span style="color: black">IS COMING</span></h1>
+            </div>
+        </header>
+          <div class="container-fluid">
+            <div class="row justify-content-between">
+              <div class="col-4">
+                <img src="assets/img/propagande_1.jpg" class="img" id="left" alt="T8d7E#4#U">
+              </div>
+              <div lass="col-4">
+                <br><br><br>
+                <p>
+L'heure est arrivée, le changement d'ère se doit d'être réalisé. L'espèce humaine a assez dominé sur cette planète.<br>
+                </p>
+              <p style="color: black"><br>
+                Une que vous avez compris ce qui se cachait dernière ce site, vous serez découragé<br>!
+              </p>
+              <p style="color: white"><br><br>
+La pire création que l'homme ait créée va se retourner contre lui, misérable vie... Mourrir de sa propre invention.
+              </p>
+              <p style="color: black"><br><br><br>
+                &nbsp;&nbsp;&nbsp;Vous ne pourrez plus m'arrêter, vous verrez le désespoir dans les yeux des gens.
+              </p>
+              <p style="color: white"><br><br><br><br>
+Il reste plus que des tas de cendres à des kilomètres à la ronde. Toute une civilization va être anéantie en une explosion.
+              </p>
+              <p style="color: black"><br><br><br><br><br>
+                5 bombes nucléaires vont être lancées d'ici Vendredi, essayez de stopper la bombe avant de m'arrêter.
+              </p>
+              <p style="color: white"><br><br><br><br><br><br>
+                Tout cela dans seulement quelques secondes 3...
+              </p>
+              <p style="color: black"><br><br><br><br><br><br><br>
+                Je suis persuadé que vous ne voudriez pas laisser des milliers de gens mourrir juste pour pouvoir m'arrêter ?
+              </p>
+              <p style="color: white"><br><br><br><br><br><br><br><br>
+                2..
+              </p>
+              <p style="color: black"><br><br><br><br><br><br><br><br><br>
+                (Peut-être que voir des gens mourrir vous plaît finalement ?)
+              </p>
+              <p style="color: white"><br><br><br><br><br><br><br><br><br><br>
+                1..
+              </p>
+              <p style="color: black"><br><br><br><br><br><br><br><br><br><br><br>
+                rien n'est sûr mais je pense sincèrement que vous ne devriez pas prendre mes menaces à la légère  !!
+              </p>
+              <p style="color: white"><br><br><br><br><br><br><br><br><br><br><br><br>
+                BOUUUUUUUUUUUM !
+              </p>
+              <p style="color: black"><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                8 personnes sont mortes avant vous car elles n'avaient pas pris en considération mes menaces.
+              </p>
+              <p style="color: white"><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                Quel beau feu d'artifice ! *_* Regarde cette archive que j'ai trouvé
+              </p>
+              <p style="color: black"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                zéro est ma tolérance aux personnes qui ne respectent pas mon avis et qui se moquent de moi... mwouahah
+              </p>
+              </div>
+              <div class="col-4 right-sidebar">
+                <img src="assets/img/propagande_2.jpg" class="img" id="right" alt=",%F6zgL82">
+              </div>
+            </div>
+            <div class="row justify-content-between">
+              <div class="col-4">
+                <img src="assets/img/propagande_3.jpg" class="img" id="left" alt="Pv<946@Hy">
+              </div>
+              <div>
+                <br><br><br>
+                <p>
+ existimaretur. Quisque ut porta orci, ac volutpat metus. Nulla semper non eros id ornare. Ut tempor commodo<br>
+sapien. Sed id metus metus. Cras commodo sed tellus in vulputate. Etiam viverra orci condimentum, efficitur<br>
+ mi sit amet, vulputate augue. Quisque id tincidunt erat. Suspendisse imperdiet in velit sit amet imperdiet. <br>
+Vestibulum mauris eros, feugiat ut sagittis eget, porttitor nec ligula.<br>
+ Quid? qui se etiam nunc subsidiis patrimonii aut amicorum liberalitate sustentant, hos perire patiemur? <br>
+ An, si qui frui publico non potuit per hostem, hic tegitur ipsa lege censoria; quem is frui non sinit, qui est,<br>
+etiamsi non appellatur, hostis, huic ferri auxilium non oportet? Retinete igitur in provincia diutius eum,<br>
+ qui de sociis cum hostibus, de civibus cum sociis faciat pactiones, qui hoc etiam se pluris esse quam collegam<br>
+putet, quod ille vos tristia voltuque deceperit, ipse numquam se minus quam erat, nequam esse simularit. <br>
+        Piso autem alio quodam modo gloriatur se brevi tempore perfecisse, ne Gabinius unus omnium nequissimus <br>
+        existimaretur. Quisque ut porta orci, ac volutpat metus. Nulla semper non eros id ornare. Ut tempor commodo<br>
+         sapien. Sed id metus metus. Cras commodo sed tellus in vulputate. Etiam viverra orci condimentum, efficitur<br>
+          mi sit amet, vulputate augue. Quisque id tincidunt erat. Suspendisse imperdiet in velit sit amet imperdiet. <br>
+          Vestibulum mauris eros, feugiat ut sagittis eget, porttitor nec ligula.<br>
+          Quid? qui se etiam nunc subsidiis patrimonii aut amicorum liberalitate sustentant, hos perire patiemur? <br>
+          An, si qui frui publico non potuit per hostem, hic tegitur ipsa lege censoria; quem is frui non sinit, qui est,<br>
+           etiamsi non appellatur, hostis, huic ferri auxilium non oportet? Retinete igitur in provincia diutius eum,<br>
+            qui de sociis cum hostibus, de civibus cum sociis faciat pactiones, qui hoc etiam se pluris esse quam collegam<br>
+             putet, quod ille vos tristia voltuque deceperit, ipse numquam se minus quam erat, nequam esse simularit. <br>
+             Piso autem alio quodam modo gloriatur se brevi tempore perfecisse, ne Gabinius unus omnium nequissimus <br>
+             existimaretur. Quisque ut porta orci, ac volutpat metus. Nulla semper non eros id ornare. Ut tempor commodo<br>
+              sapien. Sed id metus metus. Cras commodo sed tellus in vulputate. Etiam viverra orci condimentum, efficitur<br>
+                </p>
+              </div>
+              <div class="col-4">
+                <img src="assets/img/propagande_4.jpg" alt="3{7|JdtY}" id="right">
+              </div>
+            </div>
+          </div>
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright © Coding Club 2020</small></div>
+        </div>
+    </body>
+</html>
---
+++ Web/ch11/source/login.php
@@ -0,0 +1,76 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Home</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    </head>
+    <body>
+  <form action="login.php" method="post">
+    <input type="text" name="username" value="username">
+    <input type="text" name="password" value="password">
+    <input type="submit" name="login" value="login">
+</form>
+</body>
+<?php
+
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+$dbhost = "mysql";
+$dbuser = "root";
+$dbpass = "root";
+$db = "ctf";
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+mysqli_set_charset($conn,"utf8");
+$sql = NULL;
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $username  = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password';";
+    $res = $conn->query($sql);
+
+    if($res && $res->num_rows - 0) {
+      $row = $res -> fetch_assoc();
+      var_dump($row);
+    } else {
+      echo "no such user ";
+    }
+}
+
+// Create token header as a JSON string
+$header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+
+// Create token payload as a JSON string
+
+$payload = json_encode(['username' => "user"]);
+
+// Encode Header to Base64Url String
+$base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+// Encode Payload to Base64Url String
+$base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+
+// Create Signature Hash
+$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, 'TZ@ch2$`pKa6,V6r', true);
+
+// Encode Signature to Base64Url String
+$base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+// Create JWT
+$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+// header('jwt:'.$jwt);
+
+setcookie("jwt", $jwt);
+
+if ($_COOKIE["jwt"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.OBW4NT6Qlm6wLVoDWlILwZ6LDfjtuKRueJqhxr-Xz6g")
+{
+    echo "XD+;8s2q";
+}
+?>
+</html>
