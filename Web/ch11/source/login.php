8196fca5aab4a1307c5fc0a21449502882ecb0a4
Lucas <lucas.marsala@epitech.eu>
2020-07-01 12:46:02 +0200

    changed jwt of summer camp ch11

--- Web/ch11/source/login.php
+++ Web/ch11/source/login.php
@@ -68,7 +68,7 @@ $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
 
 setcookie("jwt", $jwt);
 
-if ($_COOKIE["jwt"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIn0.OBW4NT6Qlm6wLVoDWlILwZ6LDfjtuKRueJqhxr-Xz6g")
+if ($_COOKIE["jwt"] == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Iml0YWthbGEifQ.c-wU_vvVqgaR9PRu6s6g5KNLvJtXMiowLZlprImzwjg")
 {
     echo "XD+;8s2q";
 }
